0d8ddb16194ea6e52d609e3099019d1468051513
pyk129 <pyk129@naver.com>
2023-05-12 15:54:49 +0900

    init

---
+++ sbuilderDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sbuilderDemo
+//
+//  Created by parkyk on 2023/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sbuilderDemo/sbuilderDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  sbuilderDemoApp.swift
+//  sbuilderDemo
+//
+//  Created by parkyk on 2023/02/21.
+//
+
+import SwiftUI
+
+@main
+struct sbuilderDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
